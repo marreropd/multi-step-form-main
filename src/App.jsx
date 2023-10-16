b1cc9921f8cfc84b58aa0168d7ac375e44ea288d
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-10-17 01:24:21 +0200

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import "./App.css";
 import MultiStepForm from "./components/MultistepForm/MultistepForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <MultiStepForm />
