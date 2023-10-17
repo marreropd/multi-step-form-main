4203d2abe85e794e2963797d4596e182d2135bbb
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-10-17 15:21:58 +0200

    add html and styles to first step form component

---
+++ src/components/FirstStepForm.jsx
@@ -0,0 +1,60 @@
+const FirstStepForm = () => {
+  return (
+    <section className="flex flex-col content-center justify-center p-6 py-9 ">
+      <div className="flex h-[100%] flex-col gap-3 rounded-xl bg-white p-8 shadow">
+        <h2 className="text-3xl font-medium text-marine-blue">Personal info</h2>
+        <p className="text-balance max-w-sm">
+          Please provide your name, email, adress, and phone number.
+        </p>
+
+        <form action="/" className="flex flex-col font-medium ">
+          <label htmlFor="name" className="block text-[.9rem] text-marine-blue">
+            Name
+          </label>
+          <input
+            className="rounded border p-2 text-[.9rem]"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="e.g Stephen King"
+            required
+          />
+
+          <label
+            htmlFor="name"
+            className="mt-3 block text-[.9rem] text-marine-blue"
+          >
+            Email Address
+          </label>
+          <input
+            className="rounded border p-2"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="e.g sephenking@lorem.com"
+            required
+          />
+
+          <label
+            htmlFor="phone"
+            className="mt-3 block text-[.9rem] text-marine-blue"
+          >
+            Phone Number
+          </label>
+          <input
+            className="rounded border p-2"
+            type="tel"
+            name="phone"
+            id="phone"
+            placeholder="e.g +1 234 567 890"
+            required
+          />
+
+          <input type="submit" id="submit-form-step-1" class="hidden" />
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default FirstStepForm;
--- src/components/MultiStepForm.jsx
+++ src/components/MultiStepForm.jsx
@@ -1,36 +1,38 @@
+import FirstStepForm from "./FirstStepForm";
 import "./MultiStepForm.css";
 
 const MultiStepForm = () => {
   return (
-    <section className="m-0 h-screen p-0 font-main-font">
+    <section className="h-screen font-main-font">
       <div className="h-[10.75rem] w-[100%] bg-[url('/assets/images/bg-sidebar-mobile.svg')] bg-cover bg-center bg-no-repeat py-10">
         <header>
           <ul className="relative flex list-none items-center  justify-center gap-7">
             <li>
-              <div className="flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-full border text-center text-xs text-white hover:bg-sky-700">
+              <div className="flex h-[2rem] w-[2rem] items-center justify-center rounded-full border text-center text-xs text-white hover:bg-sky-700">
                 1
               </div>
             </li>
             <li>
-              <div className="flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-full border text-center text-xs text-white">
+              <div className="flex h-[2rem] w-[2rem] items-center justify-center rounded-full border text-center text-xs text-white">
                 2
               </div>
             </li>
             <li>
-              <div className="flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-full border text-center text-xs text-white">
+              <div className="flex h-[2rem] w-[2rem] items-center justify-center rounded-full border text-center text-xs text-white">
                 2
               </div>
             </li>
             <li>
-              <div className="flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-full border text-center text-xs text-white">
+              <div className="flex h-[2rem] w-[2rem] items-center justify-center rounded-full border text-center text-xs text-white">
                 4
               </div>
             </li>
           </ul>
         </header>
-        <section className="flex flex-col content-center justify-center p-6 py-9">
-          <div className="h-[25rem] rounded-xl bg-black"></div>
-        </section>
+        <FirstStepForm />
+        {/*   <label for="submit-form-step-1" tabindex="0">
+          Next Step
+        </label> */}
       </div>
 
       {/*       <p className="bg-marine-blue text-light-blue">hola</p> */}
